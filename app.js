1d068e3e5f9819b6c21fdc961831ad8ff1c9da90
TRAVEN\Traven <379673252@qq.com>
2016-12-27 15:08:13 +0800

    echostr

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let Koa = require('koa');
 let sha1 = require('sha1');
 let config = {
-    webchat:{
+    wechat:{
         appID:'wxb72c464bf26777ad',
         appsecret:'6fdb9dfafa6a7a07eb3c413a0e79cb26',
         token:'q1w'
@@ -11,7 +11,20 @@ let config = {
 let app = new Koa();
 app.use(function *(next){
     console.log(this.query);
-
+    console.log(this.query.ecostr);
+    let token = config.wechat.token;
+    let signature = this.query.signature;
+    let nonce = this.query.nonce;
+    let timestamp = this.query.timestamp;
+    let echostr = this.query.echostr;
+    let str = [token,timestamp,nonce].sort().join('');
+    let sha = sha1(str);
+    if(sha === signature){
+        console.log(echostr);
+        this.body = echostr+'';
+    }else{
+        this.body = 'wrong';
+    }
 });
 app.listen(3100);
 console.log('start 3100');
