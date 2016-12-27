7737f5202febbd630f5c05fff1a80dc398ce9b12
TRAVEN\Traven <379673252@qq.com>
2016-12-28 00:10:30 +0800

    upload template

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -3,8 +3,10 @@ let prefix = 'https://api.weixin.qq.com/cgi-bin';
 let Promise = require('bluebird');
 let request = Promise.promisify(require('request'));
 let util = require('./util');
+let fs = require('fs');
 let api = {
-    accessToken: prefix + '/token?grant_type=client_credential'
+    accessToken: prefix + '/token?grant_type=client_credential',
+    upload:prefix+'/media/upload?'
 };
 function Wechat(opts) {
     let that = this;
@@ -12,6 +14,15 @@ function Wechat(opts) {
     this.appsecret = opts.appsecret;
     this.getAccessToken = opts.getAccessToken;
     this.saveAccessToken = opts.saveAccessToken;
+    this.fetchAccessToken();
+}
+Wechat.prototype.fetchAccessToken = function (data) {
+    let that = this;
+    if(this.access_token && this.expires_in){
+        if(this.isValidAccessToken(this)){
+            return Promise.resolve(this);
+        }
+    }
     this.getAccessToken().then(function (data) {
         try {
             data = JSON.parse(data);
@@ -28,8 +39,9 @@ function Wechat(opts) {
         that.expires_in = data.expires_in;
         console.log(data,'data1');
         that.saveAccessToken(data);
+        return Promise.resolve(data);
     });
-}
+};
 Wechat.prototype.isValidAccessToken = function (data) {
     if (!data || !data.access_token || !data.expires_in) {
         return false;
@@ -40,6 +52,7 @@ Wechat.prototype.isValidAccessToken = function (data) {
     if (now < expires_in) return true;
     else return false;
 };
+
 Wechat.prototype.updateAccessToken = function () {
     let appID = this.appID;
     let appsecret = this.appsecret;
@@ -52,6 +65,29 @@ Wechat.prototype.updateAccessToken = function () {
             data.expires_in = expires_in;
             console.log(data);
             resolve(data);
+        });
+    });
+};
+Wechat.prototype.uploadMaterial = function (type,filePath) {
+    let that = this;
+    let form = {
+        media:fs.createReadStream(filePath)
+    };
+    return new Promise(function (resolve, reject) {
+        that.fetchAccessToken()
+        .then(function(data){
+            let url = api.upload + '&access_token=' + data.access_token + '&type=' + type;
+            request({method:'POST',url: url,formData:form, json: true}).then(function (response) {
+                let _data = response[1];
+                console.log(_data);
+                if(_data){
+                    resolve(_data);
+                }else {
+                    throw new Error('Upload material fails');
+                }
+            }).catch(function(err){
+                reject(err);
+            });
         });
     });
 };
--- weixin.js
+++ weixin.js
@@ -1,4 +1,7 @@
 'use strict';
+let config = require('./config');
+let Wechat = require('./wechat/wechat');
+let wechatApi = new Wechat(config.wechat);
 exports.reply = function* (next){
     let message = this.weixin;
     if(message.MsgType === 'event'){
@@ -10,6 +13,62 @@ exports.reply = function* (next){
         }else if(message.Event === 'unsubscribe'){
             console.log('无情取关');
             this.body = '';
+        }else if(message.Event === 'LOCATION'){
+            this.body = '上报的位置是： '+message.Latitude+'/'+message.Longitude+'-'+message.Precision;
+        }else if(message.Event === 'CLICK'){
+            this.body = '点击了菜单'+message.EventKey;
+        }else if(message.Event === 'SCAN'){
+            console.log('关注后扫二维码'+message.EventKey+message.Ticket);
+            this.body = '看到你扫了一下哦';
+        }else if(message.Event === 'VIEW'){
+            console.log('关注后扫二维码'+message.EventKey+message.Ticket);
+            this.body ='点击了菜单中的链接'+message.EventKey;
         }
+    }else if(message.MsgType === 'text'){
+        let content = message.Content;
+        let reply = '额,你说的 '+content+' 太复杂了';
+        if(content === '1'){
+            reply = '你说的是 '+content;
+        }else if(content === '2'){
+            reply = '你说的是你说的是 '+content;
+        }else if(content === '3'){
+            reply = '你说的是你说的是你说的是 '+content;
+        }else if(content === '4'){
+            reply = [{
+                title:'嘟嘟嘟嘟有多帅',
+                description:'只是一个小问题',
+                picUrl:'http://wanzao2.b0.upaiyun.com/system/pictures/29448439/original/1444383361_452x281.png'
+            }];
+        }else if(content === '5'){
+            reply = [{
+                title:'尤娇寿考上研究僧了吗？',
+                description:'必须可以',
+                picUrl:'http://comment.b0.upaiyun.com/system/comments/attachments/037/407/180/original/1451751663-320x320.png'
+            }];
+        }else if(content === '6'){
+            let data = yield wechatApi.uploadMaterial('image',__dirname+'/2.jpg');
+            reply = {
+                type:'image',
+                mediaId:data.media_id
+            };
+        }else if(content === '7'){
+            let data = yield wechatApi.uploadMaterial('video',__dirname+'/6.mp4');
+            reply = {
+                type:'video',
+                title:'回复视频内容',
+                description:'测试视频',
+                mediaId:data.media_id
+            };
+        }else if(content === '8'){
+            let data = yield wechatApi.uploadMaterial('image',__dirname+'/2.jpg');
+            reply = {
+                type:'music',
+                title:'回复音乐内容',
+                description:'放松一下',
+                musicUrl:'http://mpge.5nd.com/2015/2015-9-12/66325/1.mp3',
+                thumbMediaId:data.media_id
+            };
+        }
+        this.body = reply;
     }
 };
